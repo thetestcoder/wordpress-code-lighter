f6a2d4ac62119942c2d9be7d6867fec981229d8c
The Test Coder <thetestcoder@gmail.com>
2021-02-08 13:37:01 +0530

    --WIP--

---
+++ code-lighter.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Plugin Name:       Code Lighter
+ * Description:       A Plugin that help you to highlight your code at front side.
+ * Version:           1.0.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.4
+ * Author:            The Test Coder
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       code-lighter
+ *
+ * Code Lighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Code Lighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Code Lighter. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+ */
+
