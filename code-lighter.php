79cf2cfe59f580c0b7e85afa67f70c8adf031f12
The Test Coder <thetestcoder@gmail.com>
2021-02-11 11:21:13 +0530

    Lowest Version Changed

--- code-lighter.php
+++ code-lighter.php
@@ -5,7 +5,7 @@
  * Description:       A Plugin that help you to highlight your code at front side.
  * Version:           1.0.0
  * Requires at least: 5.6
- * Requires PHP:      7.4
+ * Requires PHP:      7.3
  * Author:            The Test Coder
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
